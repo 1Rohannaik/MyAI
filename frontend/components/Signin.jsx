9285ff3e40f97edb9a42b2b1ffaeb0f2329ea5aa
Rohan Naik <139874770+1Rohannaik@users.noreply.github.com>
2025-04-09 22:05:10 +0530

    Update Signin.jsx

--- frontend/components/Signin.jsx
+++ frontend/components/Signin.jsx
@@ -25,7 +25,7 @@ const Signin = () => {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:3000/api/v1/users/signup",
+        "https://myai-backend-i80t.onrender.com/api/v1/users/signup",
         {
           method: "POST",
           headers: {
